606e1ad4dafd3a19d197dc20a713ed1539a6c10f
Wes Freeman <wes@skeweredrook.com>
2013-12-20 02:30:05 -0500

    fixing benchmarks

--- map_bench_test.go
+++ map_bench_test.go
@@ -14,46 +14,43 @@ func (s *MapBenchSuite) BenchmarkPut(c *C) {
 	for ; i < c.N; i++ {
 		m.Put(i, i)
 	}
-	c.Assert(m.Len(), Equals, i+1)
+	c.Assert(m.Len(), Equals, i)
 }
 
 func fillMap(n int) *Map {
 	m := NewMap(compareInts)
 	for j := 0; j < n; j++ {
-		m.Put(j, j)
+		m.Put(j, j*2)
 	}
 	return m
 }
 
+func benchmarkGetN(n int, c *C) {
+	m := fillMap(n)
+	i := 0
+	for ; i < c.N; i++ {
+		x, ok := m.Get(i)
+		if i < n {
+			c.Assert(ok, Equals, true)
+			c.Assert(x, Equals, i*2)
+		} else {
+			c.Assert(ok, Equals, false)
+			c.Assert(x, IsNil)
+		}
+	}
+}
+
 func (s *MapBenchSuite) BenchmarkGet1000(c *C) {
-	m := fillMap(1000)
-	i := 0
-	for ; i < c.N; i++ {
-		x, ok := m.Get(i)
-		c.Assert(ok, Equals, true)
-		c.Assert(x, Equals, i*2)
-	}
-	c.Assert(m.Len(), Equals, i+1)
+	benchmarkGetN(1000, c)
 }
-
 func (s *MapBenchSuite) BenchmarkGet10000(c *C) {
-	m := fillMap(10000)
-	i := 0
-	for ; i < c.N; i++ {
-		x, ok := m.Get(i)
-		c.Assert(ok, Equals, true)
-		c.Assert(x, Equals, i*2)
-	}
-	c.Assert(m.Len(), Equals, i+1)
+	benchmarkGetN(10000, c)
 }
-
 func (s *MapBenchSuite) BenchmarkGet100000(c *C) {
-	m := fillMap(100000)
-	i := 0
-	for ; i < c.N; i++ {
-		x, ok := m.Get(i)
-		c.Assert(ok, Equals, true)
-		c.Assert(x, Equals, i*2)
-	}
-	c.Assert(m.Len(), Equals, i+1)
+	c.Skip("too slow")
+	benchmarkGetN(100000, c)
+}
+func (s *MapBenchSuite) BenchmarkGet1000000(c *C) {
+	c.Skip("too slow")
+	benchmarkGetN(1000000, c)
 }
